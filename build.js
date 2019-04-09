a0856baebf6e605a01ff53d9d1d80dc11ee097fb
Eric Berdahl <berdahl@serendipity.org>
2019-04-09 10:59:01 -0700

    Converted schedule page to metalsmith/handlebars.

--- build.js
+++ build.js
@@ -13,19 +13,45 @@ const Metalsmith = require('metalsmith');
 const collections = require('metalsmith-collections');
 const debug = require('metalsmith-debug-ui');
 const discoverPartials = require('metalsmith-discover-partials');
+const Handlebars = require('handlebars');
 const helpers = require('handlebars-helpers');
 const inplace = require('metalsmith-in-place');
 const metadata = require('./lib/metadata');
+const path = require('path');
 const prefix = require('metalsmith-prefixoid');
+const serve = require('metalsmith-serve');
 const tidy = require('metalsmith-html-tidy');
 
-helpers();
+const options = {
+    basePath: '/group/SymCh',
+    tidy: true,
+    serve: false,
+}
 
-const ms = Metalsmith(__dirname);
+helpers({
+    handlebars: Handlebars
+});
 
-debug.patch(ms);
+Handlebars.registerHelper('pathJoin', function() {
+    const segments = Array.prototype.slice.call(arguments, 0, -1);
+    return path.join.apply(path, segments);
+});
 
-ms.source('./source')
+
+Handlebars.registerHelper('arr', function() {
+    // Return arguments as an array, ommiting the final (options) item
+    return Array.prototype.slice.call(arguments, 0, -1);
+});
+
+Handlebars.registerHelper('concat', (first, second) => {
+    return first.concat(second);
+});
+
+let metalsmith = Metalsmith(__dirname);
+
+debug.patch(metalsmith);
+
+metalsmith = metalsmith.source('./source')
     .destination('./build')
     .clean(true)
     .use(discoverPartials({
@@ -39,13 +65,18 @@ ms.source('./source')
     .use(collections())
     .use(inplace({
         pattern: '**/*.hbs',
-    }))
-    .use(prefix([
-        { prefix: '/group/SymCh', tag: 'a', attr: 'href' },
-        { prefix: '/group/SymCh', tag: 'area', attr: 'href' },
-        { prefix: '/group/SymCh', tag: 'img', attr: 'src' },
-        { prefix: '/group/SymCh', tag: 'link', attr: 'href' },
-        { prefix: '/group/SymCh', tag: 'script', attr: 'src' },
+    }));
+
+if (options.serve) {
+    metalsmith = metalsmith.use(serve());
+}
+else {
+    metalsmith = metalsmith.use(prefix([
+        { prefix: options.basePath, tag: 'a', attr: 'href' },
+        { prefix: options.basePath, tag: 'area', attr: 'href' },
+        { prefix: options.basePath, tag: 'img', attr: 'src' },
+        { prefix: options.basePath, tag: 'link', attr: 'href' },
+        { prefix: options.basePath, tag: 'script', attr: 'src' },
     ]))
     .use(tidy({
         tidyOptions: {
@@ -57,7 +88,9 @@ ms.source('./source')
             'tidy-mark': false,
             'vertical-space': false,
         }
-    }))
-    .build((err) => {
+    }));
+}
+
+metalsmith.build((err) => {
         if (err) throw err;
     });
