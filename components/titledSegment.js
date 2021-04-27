4a05213d297461491695e167b6a0b7b7e3597f0d
Eric Berdahl <eberdahl@adobe.com>
2021-04-27 11:13:43 -0700

    Styled TitledSegment

--- components/titledSegment.js
+++ components/titledSegment.js
@@ -3,8 +3,8 @@ import styles from '../styles/titledSegment.module.scss'
 export default function TitledSegment(props) {
     return (
         <div className={styles.segment}>
-            <h2>{props.title}</h2>
-            <div>{props.children}</div>
+            <h2 className={styles.title}>{props.title}</h2>
+            <div className={styles.content}>{props.children}</div>
         </div>
     );
 }
