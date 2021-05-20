b02cc0bd05525123b288871839320ca677c67c34
Eric Berdahl <eberdahl@adobe.com>
2021-05-19 20:47:50 -0700

    Added keys to breadcrumb list items

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -1,19 +1,18 @@
 import Link from 'next/link'
 
+import { Fragment } from 'react'
+
 import styles from '../styles/breadcrumbs.module.scss'
 
-export default function Breadcrumbs(props) {
-    const paths = props.path.map((item) => {
-        return (item[0] == '' ? 
-            (<>{item[1]}</>) :
-            (<><Link href={item[0]}><a>{item[1]}</a></Link>  » </>));
-
-    });
-
+export default function Breadcrumbs({ path }) {
     return (
         <div className={styles.breadcrumb}>
             <p>
-                {paths}
+                {path.map((item) => {
+                        return (item[0] == '' ? 
+                            (<Fragment key={item[1]}>{item[1]}</Fragment>) :
+                            (<Fragment key={item[1]}><Link href={item[0]}><a>{item[1]}</a></Link>  » </Fragment>));
+                }) }
             </p>
         </div>
     )
