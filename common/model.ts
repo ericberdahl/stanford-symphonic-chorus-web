4466283d8e250d6c198a8c869f93b7a7d80980ef
Eric Berdahl <eberdahl@adobe.com>
2021-09-24 12:34:02 -0700

    Pulled global configuration into the Model proper

--- common/model.ts
+++ common/model.ts
@@ -7,36 +7,38 @@ import yaml from 'yaml';
 
 import Performance from './performance'
 
-const CONFIG_FILENAME = path.join('data', 'main.yml');
+const CONFIG_FILENAME       = path.join('data', 'main.yml');
+const PERFORMANCE_DATA_DIR  = path.join('data', 'performances')
 
 type Configuration = {
     timezone : string;          // name of timezone in which the ensemble rehearses and performs
     currentQuarter : string;    // name of the current quarter being prepared by the ensemble
 }
 
-interface IModel {
+export interface IModel {
     readonly performances : Performance[];
     readonly currentQuarter : Performance;
-    readonly config : Configuration;
+    readonly timezone : string;
 }
 
 export default class Model implements IModel {
-    readonly performances : Performance[]       = [];
-    private currentPerformance : Performance    = null;
-    readonly config : Configuration;
+    readonly performances : Performance[]   = [];
+    private _currentQuarter : Performance   = null;
+    private config : Configuration;
 
     constructor(config : Configuration) {
         this.config = config;
     }
 
-    get currentQuarter() { return this.currentPerformance; }
+    get currentQuarter() { return this._currentQuarter; }
+    get timezone() { return this.config.timezone; }
 
     addPerformance(p : Performance) {
         this.performances.push(p);
         this.performances.sort((a, b) => b.firstConcert.start.diff(a.firstConcert.start).toMillis());
     
         if (p.quarter == this.config.currentQuarter) {
-            this.currentPerformance = p;
+            this._currentQuarter = p;
         }
 }
 
@@ -72,17 +74,14 @@ export default class Model implements IModel {
 
         const model = new Model(<Configuration>yaml.parse(await fs.readFile(path.join(basePath, CONFIG_FILENAME), 'utf8')));
 
-        const performanceOptions = {
-            timezone: model.config.timezone
-        }
-
-        const dirEntries = await fs.readdir(path.join(basePath, 'data', 'performances'), { withFileTypes: true });
+        const performanceDataDirFullPath = path.join(basePath, PERFORMANCE_DATA_DIR);
+        const dirEntries = await fs.readdir(performanceDataDirFullPath, { withFileTypes: true });
         const performancesEntries = dirEntries.filter((dirent) => dirent.isFile())
                                         .map((dirent) => dirent.name);
         const performances = await Promise.all(performancesEntries.map(async (filename) => {            
-            const filepath = path.join(basePath, 'data', 'performances', filename);
+            const filepath = path.join(performanceDataDirFullPath, filename);
             const contents = await fs.readFile(filepath, 'utf8');
-            const performance = Performance.deserialize(yaml.parse(contents), performanceOptions);
+            const performance = Performance.deserialize(yaml.parse(contents), model);
 
             model.addPerformance(performance);
 
