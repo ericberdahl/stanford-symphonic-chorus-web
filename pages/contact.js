a877d1458612935f00363b1278916373434eb4fe
Eric Berdahl <eberdahl@adobe.com>
2021-09-27 10:03:57 -0700

    HTML slimming for exported pages Added unstable_runtimeJS=false to all pages, suppressing dynamic Next.js behavior in exported pages.

--- pages/contact.js
+++ pages/contact.js
@@ -45,3 +45,12 @@ export default function Contact() {
         </Layout>
     );
 }
+
+// Setting unstable_runtimeJS to false removes all Next.js/React scripting from
+// the page when it is exported. This creates a truly static HTML page, but
+// should be used with caution because it does remove all client-side React
+// processing. So, the page needs to be truly static, or the export will yield
+// a page that is incorrect.
+export const config = {
+    unstable_runtimeJS: false
+};
--- pages/performances/[id].js
+++ pages/performances/[id].js
@@ -380,3 +380,12 @@ export async function getStaticPaths() {
 
     return result;
 }
+
+// Setting unstable_runtimeJS to false removes all Next.js/React scripting from
+// the page when it is exported. This creates a truly static HTML page, but
+// should be used with caution because it does remove all client-side React
+// processing. So, the page needs to be truly static, or the export will yield
+// a page that is incorrect.
+export const config = {
+    unstable_runtimeJS: false
+};
