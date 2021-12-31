f80768aeb39a3bab9b48a0de5a2b1635fa1fe135
Eric Berdahl <eberdahl@adobe.com>
2021-12-30 17:21:07 -0800

    Initial pass as fslightbox.js based lightbox code
    https://fslightbox.com/

--- components/lightbox.js
+++ components/lightbox.js
@@ -1,8 +1,47 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import Script from 'next/script'
 
 import styles from '../styles/lightbox.module.scss'
+
+let lightboxCount = 0;
+
+function ImgLightbox({ label, caption, image, thumb, thumb_width, thumb_height }) {
+    return (
+        <a
+            data-fslightbox={label}
+            data-caption={caption}
+            href={image}>
+            <img
+                src={thumb}
+                width={thumb_width}
+                height={thumb_height}
+                alt={caption}/>
+        </a>
+    );
+}
+
+function PdfLightbox({ label, caption, image, thumb, thumb_width, thumb_height, image_width, image_height }) {
+    return (
+        <>
+            <a
+                data-fslightbox={label}
+                data-caption={caption}
+                href={"#" + label}>
+                <img
+                    src={thumb}
+                    width={thumb_width}
+                    height={thumb_height}
+                    alt={caption} />
+            </a>
+            <div style={{display: 'none '}}>
+                <iframe
+                    id={label}
+                    src={image}
+                    width={image_width}
+                    height={image_height} />
+            </div>
+        </>
+    );
+}
 
 export default function Lightbox(props) {
     const display = (props.display ? props.display : props.image);
@@ -12,21 +51,32 @@ export default function Lightbox(props) {
 
     const rel = "mediabox[" + props.width + " " + props.height + "]";
 
+    const galleryLabel = "" + lightboxCount++ + "-image";
+
+    const imageIsPdf = props.image.toLowerCase().endsWith(".pdf");
+    const LightboxRenderer = (imageIsPdf ? PdfLightbox : ImgLightbox);
+
+    // TODO: explore getting pro license for fslightbox, to allow captions and thumbs
+
     return (
         <div className={styles.lightbox}>
             <Head>
-                <link href="/css/slimbox.css"  type="text/css" rel="stylesheet" key="slimbox.css" />
+                <script
+                    defer
+                    src="https://cdnjs.cloudflare.com/ajax/libs/fslightbox/3.3.1/index.min.js"
+                    integrity="sha512-EqNNJuepkw5P9vxCml8eBk7C4Ld+4kAnvzOD/jG21rkxWPILGoQa5EvD62UieiJF0u3xoQrcVnce4i83VnYj/Q=="
+                    crossorigin="anonymous"
+                    referrerpolicy="no-referrer" />
             </Head>
-            <Script src="/scripts/mediabox.js" strategy="beforeInteractive"/>
-            <Link href={props.image} src={display}>
-                <a title={props.caption} rel={rel}>
-                    <img
-                        src={display}
-                        width={img_width}
-                        height={img_height}
-                        alt={props.caption}/>
-                </a>
-            </Link>
+            <LightboxRenderer
+                label={galleryLabel}
+                caption={props.caption}
+                image={props.image}
+                thumb={display}
+                thumb_width={img_width}
+                thumb_height={img_height}
+                image_width={props.width}
+                image_height={props.height} />
         </div>
     );
 }
