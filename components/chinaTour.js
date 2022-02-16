2cbdacc4692b21cf991d2d4a2884f2ca8f15a290
Eric Berdahl <eberdahl@adobe.com>
2022-02-15 21:26:52 -0800

    Misc layout fixes
    • Added SupremeHarmony picture to ChinaTour component
    • Fixed missing margin below header of AboutUs component
    • Fixed missing margin between composer entries in FYLP index page

--- components/chinaTour.js
+++ components/chinaTour.js
@@ -1,3 +1,4 @@
+import Lightbox from '../components/lightbox'
 import TitledSegment from '../components/titledSegment'
 
 import styles from '../styles/chinaTour.module.scss'
@@ -6,18 +7,21 @@ export default function ChinaTour() {
     return (
         <TitledSegment title="Coverage of the 2008 China Music Tour">
             <div className={styles.chinaTour}>
-                <p>
-                    Stanford Alumni Magazine
-                    The Alumni magazine featured a story.
-                </p>
-                <p>
-                    Stanford Chamber Chorale 2008 China Tour
-                    Photos by members of the Stanford Chamber Chorale.
-                </p>
-                <p>
-                    Flickr Photos
-                    Photos posted by tour participants
-                </p>
+                <Lightbox
+                    image="/images/SupremeHarmony.jpg"
+                    width={600}
+                    height={450}
+                    caption="Near the Gate of Supreme Harmony at the Forbidden City in Beijing. Photo by Tom Sharp."
+                    img_width={149}/>
+
+                <h3><a href="http://www.stanfordalumni.org/news/magazine/2008/sepoct/farm/news/chinatour.html">Stanford Alumni Magazine</a></h3>
+                <p>The Alumni magazine featured a story.</p>
+
+                <h3><a href="http://chorale.stanford.edu/2008_tour_china/index.html">Stanford Chamber Chorale 2008 China Tour</a></h3>
+                <p>Photos by members of the Stanford Chamber Chorale.</p>
+
+                <h3><a href="http://www.flickr.com/groups/840050@N22/">Flickr Photos</a></h3>
+                <p>Photos posted by tour participants</p>
             </div>
         </TitledSegment>
     );
