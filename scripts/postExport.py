e30dea8ec807aa4828e6572d44cdcb6c66fcf732
Eric Berdahl <eberdahl@adobe.com>
2022-09-27 20:05:14 -0700

    Added post-export trim processor

---
+++ scripts/postExport.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+import pathlib
+import shutil
+
+BASE_DIR = pathlib.Path(os.path.dirname(os.path.realpath(__file__))).parent / 'out'
+
+def removeOptionally(path):
+    try:
+        os.remove(path)
+    except (FileNotFoundError):
+        print(f'{path} did not exit')
+
+def removeDirsExcept(dirPath, subDirsToKeep):
+    subDirsToKeep = list(map(lambda s: pathlib.Path(s), subDirsToKeep))
+    for child in dirPath.iterdir():
+        if (child.relative_to(dirPath) not in subDirsToKeep):
+            shutil.rmtree(child)
+
+def main():
+    removeOptionally(BASE_DIR / '404.html')
+    removeDirsExcept(BASE_DIR / '_next', ['static'])
+    removeDirsExcept(BASE_DIR / '_next' / 'static', ['css', 'media'])
+    # sanity check that no other references to _next/ exist in the remaining files
+
+if __name__ == '__main__':
+    main()
