64c6a3e002e3904ee61fcaddd14770834c61177f
Eric Berdahl <eberdahl@adobe.com>
2023-04-03 20:12:51 -0700

    Updated covid mitigation policy

--- components/covid19MitigationPolicy.js
+++ components/covid19MitigationPolicy.js
@@ -4,7 +4,9 @@ export default function Covid19MitigationPolicy() {
             <h3>COVID-19 Mitigation Policy</h3>
             <div>
                 <p>
-                    Membership in the Stanford Symphonic Chorus requires proof of full vaccination and masking at all times for rehearsals and performances.
+                    Membership in the Stanford Symphonic Chorus requires proof of full vaccination for participation.
+                    Masking is highly recommended but not required.
+                    Those who prefer to mask are most welcome to do so.
                     These requirements may change over time with the always-shifting nature of health and safety requirements as set forth by the university.
                 </p>
             </div>
